13a0223ad6a3d18c208a82deee0deb226b70e215
Samuel Vincent Creshal <samuel@creshal.de>
2014-01-11 18:12:23 +0100

    Minor art fix

--- share/pixmaps/battery/generate-icons.sh
+++ share/pixmaps/battery/generate-icons.sh
@@ -4,3 +4,7 @@ for state in crit full high low; do
 	composite -gravity South .ac.png battery-${state}.png battery-${state}-ac.png
 	composite -gravity NorthEast .charging.png battery-${state}-ac.png battery-${state}-charging-ac.png
 done
+
+for file in *.png; do
+	optipng -silent -o9 $file
+done
